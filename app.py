c3d31808f8649f4827add623a0dbd6e4e0cf30a4
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-29 09:25:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,38 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-#for index, item in enumerate(data):
-   # print (f"{index}:{item["title"]}")
-
-#x= int(input("Year After: "))
-#print(x)
-
+"""
+#File 1
 for index, item in enumerate(data):
-    if {index}:{item["year"]} > x:
+    print (f"{index}:{item["title"]}")
+
+#File 2
+x= int(input("Year After: "))
+for index, item in enumerate(data):
+    if  item["year"] > x :
+        print (f"{index}:{item["title"]}, {item["year"]}")
+
+#File 3
+x= int(input("Year After: "))
+for index, item in enumerate(data):
+    if  item["year"] > x :
+        print (f"{index}:{item["title"]}, {item["year"]}")
+
+
+y= int(input("Year Before: "))
+for index, item in enumerate(data):
+    if  item["year"] < y :
+        print (f"{index}:{item["title"]}, {item["year"]}")
+
+#File 4
+z= int(input("Year made in: "))
+for index, item in enumerate(data):
+    if  item["year"] == z :
+        print (f"{index}:{item["title"]}, {item["year"]}")
+"""
+
+#File 5
+a= int(input("Movie name: "))
+for index, item in enumerate(data):
+    if  item["year"] == a :
         print (f"{index}:{item["title"]}")
-        
-    
